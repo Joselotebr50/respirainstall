cf500c00ac529688a95f49f3bb89b8bce6242e33
Joselotebr50 <joselo1509@gmail.com>
2026-09-10 00:37:24 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // instantaneamente e funcionar mesmo com internet ruim/instável.
 // Atenção: sempre que os arquivos do app forem atualizados, mude o CACHE_VERSION
 // abaixo para forçar o navegador a buscar os arquivos novos.
-const CACHE_VERSION = 'respira-v4';
+const CACHE_VERSION = 'respira-v5';
 
 const APP_SHELL = [
   './',
